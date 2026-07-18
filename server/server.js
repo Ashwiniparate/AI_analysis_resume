ae8ccdf1d740f22e89687450c0c3537e2d24b771
Ashwini Parate <your-email@gmail.com>
2026-07-18 11:42:19 +0530

    Fix Render port binding

--- server/server.js
+++ server/server.js
@@ -1,43 +1,27 @@
-require("dotenv").config();
-
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-
-const historyRoutes = require("./routes/historyRoutes");
-const resumeRoutes = require("./routes/resumeRoutes");
-const aiRoutes = require("./routes/aiRoutes");
 
 const app = express();
 
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
 
-// Routes
-app.use("/api/history", historyRoutes);
-app.use("/api/resume", resumeRoutes);
-app.use("/api/ai", aiRoutes);
+app.get("/", (req,res)=>{
+    res.send("AI Resume API Running");
+});
 
 
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    serverSelectionTimeoutMS: 10000,
-  })
-  .then(() => {
-    console.log("MongoDB Connected");
-  })
-  .catch((err) => {
-    console.log("MongoDB Error:", err.message);
-  });
+app.get("/health",(req,res)=>{
+    res.status(200).json({
+        status:"OK"
+    });
+});
 
 
-// Server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+app.listen(PORT,"0.0.0.0",()=>{
+    console.log(`Server running on port ${PORT}`);
 });
